99f98f306f042435c437adc8965071ab71d86b20
Michael <39930952+Rotinx@users.noreply.github.com>
2021-09-11 14:00:04 +1200

    navbar changes

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -31,8 +31,7 @@
       </div>
       <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
         <div class="flex-shrink-0 flex items-center">
-          <img class="block lg:hidden h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" alt="Workflow">
-          <img class="hidden lg:block h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg" alt="Workflow">
+          <img class="h-8 w-auto" src="@/assets/logo.png" alt="Workflow">
         </div>
         <div class="hidden sm:block sm:ml-6">
           <div class="flex space-x-4">
