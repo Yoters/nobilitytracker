111ec577a4e7d9a60f22b7450b4d5d76dc4f7b19
Tony Costanzo <tonycos92@gmail.com>
2021-09-10 22:08:12 -0500

    Update Stats.vue

--- src/components/Stats.vue
+++ src/components/Stats.vue
@@ -3,7 +3,7 @@
     <h3 class="text-lg leading-6 font-medium text-gray-900">
       Nobility Stats:
     </h3>
-    <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
+    <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-4">
       <div v-for="item in stats" :key="item.name" class="px-4 py-5 bg-white shadow rounded-lg overflow-hidden sm:p-6">
         <dt class="text-sm font-medium text-gray-500 truncate">
           {{ item.name }}
@@ -18,7 +18,8 @@
 
 <script>
 const stats = [
-  { name: 'Current Price', stat: '$0.00106969' },
+  { name: 'Current Price', stat: '$0.00968' },
+  { name: 'Market Cap', stat: '$69.69M' },
   { name: '24hr Volume', stat: '$500,000' },
   { name: '24hr Change', stat: '24.57%' },
 ]
