1f763fb95674b5da145b062a1ee839c911c5d849
Tony Costanzo <tonycos92@gmail.com>
2021-09-10 23:15:01 -0500

    Market Cap Formating

--- src/components/Stats.vue
+++ src/components/Stats.vue
@@ -5,7 +5,7 @@
     </h3>
     <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-4">
       <Stat v-bind:name="'Current Price'" v-bind:value="'$' + this.price" />
-      <Stat v-bind:name="'Market Cap'" v-bind:value="'$' + this.marketcap" />
+      <Stat v-bind:name="'Market Cap'" v-bind:value="'$' + this.marketcap + 'M'" />
       <Stat v-bind:name="'24hr Volume'" v-bind:value="'$' + this.volume24hUSD" />
       <Stat v-bind:name="'24hr Change'" v-bind:value="this.priceChange24h + '%'" />
     </dl>
