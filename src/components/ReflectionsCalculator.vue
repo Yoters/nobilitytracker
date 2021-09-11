42031c2d17855b487256b18c11565d78b7302227
Tony Costanzo <tonycos92@gmail.com>
2021-09-11 03:15:47 -0500

    Update ReflectionsCalculator.vue

--- src/components/ReflectionsCalculator.vue
+++ src/components/ReflectionsCalculator.vue
@@ -19,14 +19,14 @@
         <dl class="grid grid-cols-1 sm:grid-cols-2">
           <div class="w-full sm:max-w-xs">
             Tokens Held:
-            <label for="email" class="sr-only">Tokens</label>
-            <input v-model="tokens" type="text" name="tokens" id="email" class=" shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" placeholder="200000"/>
+            <label for="email" class="sr-only">tokens</label>
+            <input v-model="tokens" type="text" name="tokens" id="email" class=" shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"/>
           </div>
 
           <div class="w-full sm:max-w-xs">
             24hr Volume:
-            <label for="email" class="sr-only">Tokens</label>
-            <input v-model="volume" v-bind:placeholder="current24HourVolume" type="text" name="volume" id="email" class=" shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"/>
+            <label for="email" class="sr-only">volume</label>
+            <input v-model="volume" type="text" name="volume" id="email" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"/>
           </div>
         </dl>
         <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-4">
@@ -49,15 +49,15 @@
 </template>
 <script>
 const stats = [
-  { name: "Daily", stat: "$100" },
-  { name: "Weekly", stat: "$700" },
-  { name: "Monthly", stat: "$3,033" },
-  { name: "Yearly", stat: "$36,400" },
+  { name: "Daily", stat: "this.daily" },
+  { name: "Weekly", stat: "this.weekly" },
+  { name: "Monthly", stat: "this.monthly" },
+  { name: "Yearly", stat: "this.yearly" },
 ];
 export default {
   data() {
     return {
-      tokens: 0,
+      tokens: 200000,
       volume: this.current24HourVolume
     }
   },
@@ -69,7 +69,7 @@ export default {
       return this.daily * 7
     },
     monthly() {
-      return this.weekly * 4
+      return this.weekly * 52 / 12
     },
     yearly() {
       return this.monthly * 12
