6c00cf8917ce35a6185563dc4e5689abab73406d
Michael <39930952+Rotinx@users.noreply.github.com>
2021-09-11 15:35:19 +1200

    computed propertys set

--- src/components/Stats.vue
+++ src/components/Stats.vue
@@ -4,34 +4,34 @@
       Nobility Stats:
     </h3>
     <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-4">
-      <div v-for="item in stats" :key="item.name" class="px-4 py-5 bg-white shadow rounded-lg overflow-hidden sm:p-6">
-        <dt class="text-sm font-medium text-gray-500 truncate">
-          {{ item.name }}
-        </dt>
-        <dd class="mt-1 text-3xl font-semibold text-gray-900">
-          {{ item.stat }}
-        </dd>
-      </div>
+      <Stat v-bind:name="'Current Price'" v-bind:value="this.price" />
+      <Stat v-bind:name="'Market Cap'" v-bind:value="this.marketcap" />
+      <Stat v-bind:name="'24hr Volume'" v-bind:value="this.volume24hUSD" />
+      <Stat v-bind:name="'24hr Change'" v-bind:value="this.priceChange24h" />
     </dl>
   </div>
 </template>
 
 <script>
-const stats = [
-  { name: 'Current Price', stat: '$0.00968' },
-  { name: 'Market Cap', stat: '$69.69M' },
-  { name: '24hr Volume', stat: '$500,000' },
-  { name: '24hr Change', stat: '24.57%' },
-]
+import Stat from '../components/internals/Stat.vue'
 
 export default {
-  computed: {
-    
+  components: {
+    Stat
   },
-  setup() {
-    return {
-      stats,
-    }
+  computed: {
+    price() {
+      return this.$store.getters.price.toFixed(7)
+    },
+    priceChange24h() {
+      return this.$store.getters.priceChange24h
+    },
+    volume24hUSD() {
+      return this.$store.getters.volume24hUSD
+    },
+    marketcap() {
+      return this.$store.getters.marketcap
+    },
   },
 }
 </script>
