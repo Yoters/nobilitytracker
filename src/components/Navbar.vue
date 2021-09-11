36a5f27ac25196a0af17a5de187f33bd38516bb8
Tony Costanzo <tonycos92@gmail.com>
2021-09-10 22:08:57 -0500

    Navbar Styling

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,29 +1,15 @@
 <template>
-  <!-- This example requires Tailwind CSS v2.0+ -->
-<nav class="bg-gray-800">
+<nav class="bg-purple-900">
   <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
     <div class="relative flex items-center justify-between h-16">
       <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
         <!-- Mobile menu button-->
-        <button type="button" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+        <button type="button" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-purple-600 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
           <span class="sr-only">Open main menu</span>
-          <!--
-            Icon when menu is closed.
 
-            Heroicon name: outline/menu
-
-            Menu open: "hidden", Menu closed: "block"
-          -->
           <svg class="h-6 w-6" v-if="!hamburger" @click.prevent="toggleHamburger" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
           </svg>
-          <!--
-            Icon when menu is open.
-
-            Heroicon name: outline/x
-
-            Menu open: "block", Menu closed: "hidden"
-          -->
           <svg class="h-6 w-6" v-if="hamburger" @click.prevent="toggleHamburger"  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
           </svg>
@@ -35,10 +21,9 @@
         </div>
         <div class="hidden sm:flex sm:ml-6 items-center">
           <div class="flex space-x-4">
-            <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-            <a href="https://nobilitytoken.com/" target="_blank" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Nobility Website</a>
-            <a href="https://twitter.com/ThePatrity" target="_blank" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Twitter</a>
-            <a href="https://github.com/Yoters/nobilitytracker" target="_blank" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Source Code</a>
+            <a href="https://nobilitytoken.com/" target="_blank" class="text-gray-300 hover:bg-purple-600 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Nobility Website</a>
+            <a href="https://twitter.com/ThePatrity" target="_blank" class="text-gray-300 hover:bg-purple-600 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Twitter</a>
+            <a href="https://github.com/Yoters/nobilitytracker" target="_blank" class="text-gray-300 hover:bg-purple-600 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Source Code</a>
           </div>
         </div>
       </div>
@@ -48,10 +33,9 @@
   <!-- Mobile menu, show/hide based on menu state. -->
   <div class="sm:hidden" id="mobile-menu" v-if="hamburger">
     <div class="px-2 pt-2 pb-3 space-y-1">
-      <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-      <a href="https://nobilitytoken.com/" target="_blank" class="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium" aria-current="page">Nobility Website</a>
-      <a href="https://twitter.com/ThePatrity" target="_blank" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Twitter</a>
-      <a href="https://github.com/Yoters/nobilitytracker" target="_blank" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Source Code</a>
+      <a href="https://nobilitytoken.com/" target="_blank" class="text-gray-300 hover:bg-purple-600 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Nobility Website</a>
+      <a href="https://twitter.com/ThePatrity" target="_blank" class="text-gray-300 hover:bg-purple-600 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Twitter</a>
+      <a href="https://github.com/Yoters/nobilitytracker" target="_blank" class="text-gray-300 hover:bg-purple-600 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Source Code</a>
     </div>
   </div>
 </nav>
