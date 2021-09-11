cf176ed96c7606937093b32eadb92a2703a363f8
Tony Costanzo <tonycos92@gmail.com>
2021-09-10 20:39:09 -0500

    Tailwind Setup

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
+<div class="app">
+<p>Test</p>
   <Navbar />
   <div>
     <p></p>
   </div>
+</div>
 </template>
-
 <script>
 import Navbar from './components/Navbar.vue'
 import axios from 'axios'
@@ -45,12 +47,4 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import { store } from './store'
+import './styles/app.css';
 
 createApp(App).use(store).mount('#app')
