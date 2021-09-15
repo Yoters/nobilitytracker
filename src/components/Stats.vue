30d7723f3c4f57f9d1bb2ed4baee7561e4c91b13
Tony Costanzo <tonycos92@gmail.com>
2021-09-15 12:28:32 -0500

    Pancake Swap Info

--- src/components/Stats.vue
+++ src/components/Stats.vue
@@ -2,14 +2,27 @@
   <div class="mt-9">
     <StatsModal ref="statsmodal" />
     <lBankModal ref="lBankModal" />
+    <PcsModal ref="PcsModal" />
     <div class="flex justify-center lg:justify-start">
-      <h3 class="text-lg leading-6 font-medium text-gray-900 dark:text-gray-300">
+      <h3 class="text-2xl leading-6 font-medium text-gray-900 dark:text-gray-300">
         Nobility Stats 
       </h3>
-    
       <svg class="ml-1 cursor-pointer" style="width:24px;height:24px" viewBox="0 0 24 24" @click.prevent="toggleModal">
         <path fill="currentColor" d="M13,9H11V7H13M13,17H11V11H13M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" />
       </svg>
+    </div>
+
+    <hr class="mt-6">
+    <div class="flex justify-center lg:justify-start mt-2">
+      <h3 class="text-lg leading-6 font-medium text-gray-900 dark:text-gray-300">Pancake Swap</h3>
+      <svg class="ml-1 cursor-pointer" style="width:24px;height:24px" viewBox="0 0 24 24" @click.prevent="togglePcsModal">
+        <path fill="currentColor" d="M13,9H11V7H13M13,17H11V11H13M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" />
+      </svg>
+      <a href="https://pancakeswap.finance/swap" target="_blank">
+        <svg style="width:24px;height:24px" viewBox="0 0 24 24" class="ml-1">
+          <path fill="currentColor" d="M14,3V5H17.59L7.76,14.83L9.17,16.24L19,6.41V10H21V3M19,19H5V5H12V3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V12H19V19Z" />
+        </svg>
+      </a>
     </div>
     <dl class="mt-5 grid grid-cols-2 gap-5 sm:grid-cols-3 mx-auto">
       <Stat v-bind:name="'Current Price'" v-bind:value="'$' + this.price" />
@@ -21,7 +34,7 @@
     </dl>
     <hr class="mt-6">
     <div class="flex justify-center lg:justify-start mt-2">
-    <h3 class="text-lg leading-6 font-medium text-gray-900 dark:text-gray-300">LBank</h3>
+      <h3 class="text-lg leading-6 font-medium text-gray-900 dark:text-gray-300">LBank</h3>
       <svg class="ml-1 cursor-pointer" style="width:24px;height:24px" viewBox="0 0 24 24" @click.prevent="togglelBankModal">
         <path fill="currentColor" d="M13,9H11V7H13M13,17H11V11H13M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" />
       </svg>
@@ -44,6 +57,7 @@
 import Stat from '../components/internals/Stat.vue'
 import StatsModal from '../components/StatsModal.vue'
 import lBankModal from '../components/lBankModal.vue'
+import PcsModal from '../components/PcsModal.vue'
 
 export default {
   data() {
@@ -54,7 +68,8 @@ export default {
   components: {
     Stat,
     StatsModal,
-    lBankModal
+    lBankModal,
+    PcsModal,
   },
   methods: {
     toggleModal() {
@@ -62,6 +77,9 @@ export default {
     },
     togglelBankModal() {
       this.$refs.lBankModal.open = true
+    },
+    togglePcsModal() {
+      this.$refs.PcsModal.open = true
     }
   },
   computed: {
